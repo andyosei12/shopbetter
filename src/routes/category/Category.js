acea63ea6dd03b439c1ac1c82e49a7edda1e9fde
Andy Osei <andrew.owusu@novanest.net>
2022-04-16 04:36:42 +0000

    create thunks for async request

--- src/routes/category/Category.js
+++ src/routes/category/Category.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/ProductCard";
+import Spinner from "../../components/spinnrer/Spinner";
 
 import "./Category.styles.scss";
 
 const Category = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
   const { category } = useParams();
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(
@@ -19,12 +24,16 @@ const Category = () => {
   return (
     <>
       <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category-container">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
     </>
   );
 };
