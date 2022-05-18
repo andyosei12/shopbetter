47b4c57b6419f197451b9d91b513b9e99d0eebe0
Andy Osei <andrew.owusu@novanest.net>
2022-05-18 03:22:43 +0000

    feat:add dynamic imports for optimization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import Home from "./routes/home/Home";
-import Navigation from "./routes/navigation/Navigation";
-import Authentication from "./routes/authentication/Authentication";
-import Shop from "./routes/shop/Shop";
-import Checkout from "./routes/checkout/Checkout";
+import Spinner from "./components/spinnrer/Spinner";
 import { checkUserSession } from "./store/user/user.action";
+
+const Home = lazy(() => import("./routes/home/Home"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/Authentication")
+);
+const Navigation = lazy(() => import("./routes/navigation/Navigation"));
+const Shop = lazy(() => import("./routes/shop/Shop"));
+const Checkout = lazy(() => import("./routes/checkout/Checkout"));
 
 function App() {
   const dispatch = useDispatch();
@@ -15,14 +19,16 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
